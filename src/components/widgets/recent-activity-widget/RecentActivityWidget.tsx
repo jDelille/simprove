3dd9a566da0390ad9798cf23433893c3af2552c5
jdelille <justind7513@gmail.com>
2026-03-24 12:41:24 -0700

    feat: no activities widget content

--- src/components/widgets/recent-activity-widget/RecentActivityWidget.tsx
+++ src/components/widgets/recent-activity-widget/RecentActivityWidget.tsx
@@ -15,63 +15,85 @@ type RecentActivityWidgetProps = {
 const RecentActivityWidget: React.FC<RecentActivityWidgetProps> = (props) => {
   const { data, isLoading } = useActivities(props.userId);
   const router = useRouter();
-  
+
   const { latestThreeActivities = [] } = data || {};
+
+  const isEmpty = !latestThreeActivities.length && !isLoading;
 
   return (
     <div className={styles.widget}>
-      {!latestThreeActivities.length && !isLoading && <p>No recent activity</p>}
-      {isLoading && <p>Loading...</p>}
-
       <div className={styles.header}>
         <p>Recent Activity</p>
       </div>
 
-      <ul>
-        {latestThreeActivities.map((activity: any) => (
-          <li key={activity.id}>
-            {activity.type === "SESSION_CREATED" && (
-              <div className={styles.activity} onClick={() => router.push(`/session/${activity.entity_id}`)}>
-                <div
-                  className={styles.icon}
-                  style={{ backgroundColor: "var(--green)", border: "1px solid var(--greenBorder)" }}
-                >
-                  <MdOutlineFileUpload color="#2aaa55" size={20} />
-                </div>
-                <div className={styles.body}>
-                  <p className={styles.title}>
-                    {activity.title}{" "}
-                    <span className={styles.date}>
-                      {activityDateFormat(activity.created_at)} ago
-                    </span>
-                  </p>
-                  <p className={styles.description}>{activity.description}</p>
-                </div>
-              </div>
-            )}
+      {isLoading && <p>Loading...</p>}
 
-            {activity.type === "BADGE_EARNED" && (
-              <div className={styles.activity} >
+      {isEmpty && !isLoading && (
+        <div className={styles.empty}>
+          <div className={styles.icon}>
+            <p>⛳</p>
+          </div>
+          <p>
+            No activity yet
+            <br />
+            Your recent activity will appear here as you use the app and earn badges!
+          </p>
+        </div>
+      )}
+
+      {!isEmpty && !isLoading && (
+        <ul>
+          {latestThreeActivities.map((activity: any) => (
+            <li key={activity.id}>
+              {activity.type === "SESSION_CREATED" && (
                 <div
-                  className={styles.icon}
-                  style={{ backgroundColor: "#FFF8E1" }}
+                  className={styles.activity}
+                  onClick={() => router.push(`/session/${activity.entity_id}`)}
                 >
-                  <FaTrophy color="#f7c04a" size={20} />
-                </div>
-                <div className={styles.body}>
-                  <div className={styles.title}>
-                    <p className={styles.name}>{activity.title}</p>
-                    <span className={styles.date}>
-                      {activityDateFormat(activity.created_at)} ago
-                    </span>
+                  <div
+                    className={styles.icon}
+                    style={{
+                      backgroundColor: "var(--green)",
+                      border: "1px solid var(--greenBorder)",
+                    }}
+                  >
+                    <MdOutlineFileUpload color="#2aaa55" size={20} />
                   </div>
-                  <p className={styles.description}>{activity.description}</p>
+                  <div className={styles.body}>
+                    <p className={styles.title}>
+                      {activity.title}{" "}
+                      <span className={styles.date}>
+                        {activityDateFormat(activity.created_at)} ago
+                      </span>
+                    </p>
+                    <p className={styles.description}>{activity.description}</p>
+                  </div>
                 </div>
-              </div>
-            )}
-          </li>
-        ))}
-      </ul>
+              )}
+
+              {activity.type === "BADGE_EARNED" && (
+                <div className={styles.activity}>
+                  <div
+                    className={styles.icon}
+                    style={{ backgroundColor: "#FFF8E1" }}
+                  >
+                    <FaTrophy color="#f7c04a" size={20} />
+                  </div>
+                  <div className={styles.body}>
+                    <div className={styles.title}>
+                      <p className={styles.name}>{activity.title}</p>
+                      <span className={styles.date}>
+                        {activityDateFormat(activity.created_at)} ago
+                      </span>
+                    </div>
+                    <p className={styles.description}>{activity.description}</p>
+                  </div>
+                </div>
+              )}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
