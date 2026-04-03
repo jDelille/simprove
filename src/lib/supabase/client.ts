83f01f79d525fe79a100cfd93b202f58b2c5ab39
jdelille <justind7513@gmail.com>
2026-04-02 21:29:18 -0700

    fix: testing client fix

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,9 +1,10 @@
-// browser client
 import { createBrowserClient } from "@supabase/ssr";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+const createClient = () =>
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  );
 
-export const createClient = () => {
-    return createBrowserClient(supabaseUrl, supabaseKey);
-}
+export const supabase = createClient();
+export { createClient };
